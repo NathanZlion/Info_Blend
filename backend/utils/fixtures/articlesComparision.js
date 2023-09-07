a07115fa29ff0a7541c94ead38e25e4270032b01
Nathnael Dereje <nathandere1357@gmail.com>
2023-09-07 22:40:22 +0300

    Modify article comparision fixture, made it json and add message and comparison attribute to the json

--- backend/utils/fixtures/articlesComparision.js
+++ backend/utils/fixtures/articlesComparision.js
@@ -1,92 +1,95 @@
-export const articlesComparision = [
-    {
-        "source": "Standard Digital News - Kenya",
-        "content": [
-            {
-                "conflicts": "false",
-                "n": "1",
-                "sentence": "The African Union (AU) Commission has expressed concern over the ongoing violent clashes in Libya."
-            },
-            {
-                "conflicts": "false",
-                "n": "2",
-                "sentence": "In a statement on Thursday, August 17, AU chairperson Moussa Faki called for the parties involved to end the hostility."
-            },
-            {
-                "conflicts": "false",
-                "n": "3",
-                "sentence": "\"Following with great concern the developments of the security situation in Tripoli, which have resulted in the loss of many lives and many more wounded. I urge all stakeholders and all military, political, and social actors to put an immediate end to all hostilities,\" Faki stated."
-            },
-            {
-                "conflicts": "false",
-                "n": "4",
-                "sentence": "The chairperson further appealed to all peacekeeping bodies to work out solutions toward reconciliation in the conflict-hit country."
-            },
-            {
-                "conflicts": "false",
-                "n": "5",
-                "sentence": "Faki said that there could be no military solution to the problems in Libya, reiterating that peace, unity and stability, and international status can only be achieved through advocacy."
-            },
-            {
-                "conflicts": "false",
-                "n": "6",
-                "sentence": "The statement follows a fierce fight that broke out in Libya on Monday that has left many dead and scores injured."
-            },
-            {
-                "conflicts": "false",
-                "n": "7",
-                "sentence": "The fight broke out between the 444 Brigade that operates in Tripoli, where Prime Minister Abdul Hamid Mohammed Dbeibah heads a government, and the Special Deterrence Force."
-            },
-            {
-                "conflicts": "false",
-                "n": "8",
-                "sentence": "The Special Deterrence Force, which controls the airport, arrested a senior commander from the 444 Brigade, Mahmoud Hamza as he attempted to travel on Monday at Tripoli airport."
-            },
-            {
-                "conflicts": "false",
-                "n": "9",
-                "sentence": "Hamza, whose detention triggered violence, was later released on Tuesday evening."
-            }
-        ]
-    },
-    {
-        "source": "Agencia Informativa Latinamericana Prensa Latina",
-        "content": [
-            {
-                "conflicts": "false",
-                "n": "1",
-                "sentence": "Through a press release, Mahamat urged the stakeholders and all military, political, and social actors to put an immediate end to the hostilities that, according to local sources, also wounded 146 people."
-            },
-            {
-                "conflicts": "false",
-                "n": "2",
-                "sentence": "It recalled the urgent need to continue the ongoing efforts toward national reconciliation, and there is no military solution to the crisis in this country."
-            },
-            {
-                "conflicts": "false",
-                "n": "3",
-                "sentence": "The document concluded that unity, peace, stability, and Libya's historic international status can only be restored through peaceful means."
-            },
-            {
-                "conflicts": "false",
-                "n": "4",
-                "sentence": "Fighting erupted after members of the Special Deterrence Force decided to arrest Mahmoud Hamza, commander of the 444th Brigade, at Maitika airport as he was on his way to Misurata city to attend a graduation ceremony."
-            },
-            {
-                "conflicts": "maybe",
-                "n": "5",
-                "sentence": ""
-            },
-            {
-                "conflicts": "maybe",
-                "n": "6",
-                "sentence": ""
-            },
-            {
-                "conflicts": "maybe",
-                "n": "7",
-                "sentence": ""
-            }
-        ]
-    }
-]
+export const articlesComparision = {
+    "message": "Successfully compared articles.",
+    "comparison": [
+        {
+            "source": "Standard Digital News - Kenya",
+            "content": [
+                {
+                    "conflicts": "false",
+                    "n": "1",
+                    "sentence": "The African Union (AU) Commission has expressed concern over the ongoing violent clashes in Libya."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "2",
+                    "sentence": "In a statement on Thursday, August 17, AU chairperson Moussa Faki called for the parties involved to end the hostility."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "3",
+                    "sentence": "\"Following with great concern the developments of the security situation in Tripoli, which have resulted in the loss of many lives and many more wounded. I urge all stakeholders and all military, political, and social actors to put an immediate end to all hostilities,\" Faki stated."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "4",
+                    "sentence": "The chairperson further appealed to all peacekeeping bodies to work out solutions toward reconciliation in the conflict-hit country."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "5",
+                    "sentence": "Faki said that there could be no military solution to the problems in Libya, reiterating that peace, unity and stability, and international status can only be achieved through advocacy."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "6",
+                    "sentence": "The statement follows a fierce fight that broke out in Libya on Monday that has left many dead and scores injured."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "7",
+                    "sentence": "The fight broke out between the 444 Brigade that operates in Tripoli, where Prime Minister Abdul Hamid Mohammed Dbeibah heads a government, and the Special Deterrence Force."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "8",
+                    "sentence": "The Special Deterrence Force, which controls the airport, arrested a senior commander from the 444 Brigade, Mahmoud Hamza as he attempted to travel on Monday at Tripoli airport."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "9",
+                    "sentence": "Hamza, whose detention triggered violence, was later released on Tuesday evening."
+                }
+            ]
+        },
+        {
+            "source": "Agencia Informativa Latinamericana Prensa Latina",
+            "content": [
+                {
+                    "conflicts": "false",
+                    "n": "1",
+                    "sentence": "Through a press release, Mahamat urged the stakeholders and all military, political, and social actors to put an immediate end to the hostilities that, according to local sources, also wounded 146 people."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "2",
+                    "sentence": "It recalled the urgent need to continue the ongoing efforts toward national reconciliation, and there is no military solution to the crisis in this country."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "3",
+                    "sentence": "The document concluded that unity, peace, stability, and Libya's historic international status can only be restored through peaceful means."
+                },
+                {
+                    "conflicts": "false",
+                    "n": "4",
+                    "sentence": "Fighting erupted after members of the Special Deterrence Force decided to arrest Mahmoud Hamza, commander of the 444th Brigade, at Maitika airport as he was on his way to Misurata city to attend a graduation ceremony."
+                },
+                {
+                    "conflicts": "maybe",
+                    "n": "5",
+                    "sentence": ""
+                },
+                {
+                    "conflicts": "maybe",
+                    "n": "6",
+                    "sentence": ""
+                },
+                {
+                    "conflicts": "maybe",
+                    "n": "7",
+                    "sentence": ""
+                }
+            ]
+        }
+    ]
+}
