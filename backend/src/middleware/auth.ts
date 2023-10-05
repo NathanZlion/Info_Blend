072d03bee6303a81f0f405dbe1c564691e2b6646
Nathnael Dereje <nathandere1357@gmail.com>
2023-10-05 13:49:17 +0300

    Add typing for auth middleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,32 +1,30 @@
 
 import jwt from 'jsonwebtoken';
+import { Response, Request, NextFunction } from "express";
 import { Types } from 'mongoose';
-import Users from '../src/models/user.model.js';
+import Users from '../models/user.model.js';
 
-const auth = async (req, res, next) => {
+const auth = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const existingUser = await getUserFromToken(req, res, next);
+        const existingUser = await getUserFromToken(req, res);
 
         if (!existingUser)
             return res.status(401).json({ message: "Unauthorized" });
 
-        // res.locals.user = existingUser;
-        req.user = existingUser;
-
+        res.locals.user = existingUser;
         next();
     } catch (error) {
-        res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: "Unauthorized" });
     }
 }
 
-export async function getUserFromToken(req, res) {
-    const token = req.headers.authorization.split(" ")[1];
-    // checks if token is present inside the header
-    if (!token) {
-        return res.status(400).json({ message: "Unauthorized, no token found." });
-    }
+export async function getUserFromToken(req: Request, res: Response) {
+    const token: string = req.headers.authorization!.split(" ")[1];
 
-    const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+    if (!token)
+        return res.status(400).json({ message: "Unauthorized, no token found." });
+
+    const { _id }: jwt.JwtPayload = jwt.verify(token, process.env.JWT_SECRET!) as jwt.JwtPayload;
     const userId = new Types.ObjectId(_id);
     const existingUser = await Users.findById(userId);
 
