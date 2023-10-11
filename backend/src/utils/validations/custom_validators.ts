60e4a9b480f8b52887226c73be27eb2ff5cb50ed
Nathnael Dereje <nathandere1357@gmail.com>
2023-10-12 00:04:58 +0300

    Fix, Clean and simplify password strength checker function

--- backend/src/utils/validations/custom_validators.ts
+++ backend/src/utils/validations/custom_validators.ts
@@ -1,48 +1,27 @@
-
-// This file contains custom validators that can be used in the backend
-
-
-/**
- * 
- * @param {string} password
- * @returns true if password is strong enough. If not false.
- * 
- * A function to check if a password is strong enough
- */
+// A function to check if a password is strong enough
 export function checkPasswordStrength(password: string): void {
-    // criteria: Password must be at least 8 characters long
+    const criteria = [
+        { regex: /[A-Z]/, message: "Password must contain at least one uppercase letter" },
+        { regex: /[a-z]/, message: "Password must contain at least one lowercase letter" },
+        { regex: /\d/, message: "Password must contain at least one digit" },
+        { regex: /[!@#$%^&*]/, message: "Password must contain at least one special character" },
+    ];
+
+    for (const criterion of criteria) {
+        if (!criterion.regex.test(password)) {
+            throw new PasswordNotStrongEnoughError(criterion.message);
+        }
+    }
+
     if (password.length < 8) {
         throw new PasswordNotStrongEnoughError("Password must be at least 8 characters long");
     }
-
-    // criteria: Password must contain at least one uppercase letter
-    if (!/[A-Z]/.test(password)) {
-        throw new PasswordNotStrongEnoughError("Password must contain at least one uppercase letter");
-    }
-
-    // criteria: Password must contain at least one lowercase letter
-    if (!/[a-z]/.test(password)) {
-        throw new PasswordNotStrongEnoughError("Password must contain at least one lowercase letter");
-    }
-
-    // criteria: Password must contain at least one digit
-    if (!/\d/.test(password)) {
-        throw new PasswordNotStrongEnoughError("Password must contain at least one digit");
-    }
-
-    // criteria: Password must contain at least one special character
-    if (!/[!@#$%^&*]/.test(password)) {
-        throw new PasswordNotStrongEnoughError("Password must contain at least one special character");
-    }
 }
 
-
-
-
-// custrom error for password not strong enough
+// Custom error for password not strong enough
 export class PasswordNotStrongEnoughError extends Error {
     constructor(message: string) {
         super(message);
-        this.name = "Password Does Not Meet Criteria";
+        this.name = "PasswordNotStrongEnoughError";
     }
 }
