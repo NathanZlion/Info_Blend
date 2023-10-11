c431038d93a73e40fa5ae6af235c91dd5bbd6697
Nathnael Dereje <nathandere1357@gmail.com>
2023-10-12 00:24:27 +0300

    Refactor code, add function getTokenFromRequest to extract token from request.

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -6,7 +6,11 @@ import Users, { UserDocument } from '../models/user.model';
 
 const auth = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const existingUser = await getUserFromToken(req);
+        const token = getTokenFromRequest(req);
+        if (!token)
+            return res.status(401).json({ message: "Unauthorized" });
+
+        const existingUser = await getUserFromToken(token);
 
         if (!existingUser)
             return res.status(401).json({ message: "Unauthorized" });
@@ -18,9 +22,16 @@ const auth = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
-export async function getUserFromToken(req: Request) : Promise<UserDocument|null>{
-    const token: string = req.headers.authorization!.split(" ")[1];
+export function getTokenFromRequest(req: Request) : string | undefined{
+    try {
+        const token: string = req.headers.authorization!.split(" ")[1];
+        return token;
+    } catch (error) {
+        return undefined;
+    }
+}
 
+export async function getUserFromToken(token: string) : Promise<UserDocument|null>{
     const { _id }: jwt.JwtPayload = jwt.verify(token, process.env.JWT_SECRET!) as jwt.JwtPayload;
     const userId = new Types.ObjectId(_id);
     const existingUser = await Users.findById(userId);
