5213a1f6d548077bc9d80ca65b982f527980037a
Yonatan <yon019hl@gmail.com>
2023-12-24 16:09:42 +0300

    finish desktop navbar

--- web/src/pages/Landing/LandingPage.jsx
+++ web/src/pages/Landing/LandingPage.jsx
@@ -1,16 +1,43 @@
-// import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+
+function OutNavbar() {
+  return (
+    <div className='px-[2rem] py-[16px] bg-gradient-to-r from-[#E47500] to-[#DF7C14] text-white'>
+      <nav className='flex justify-between items-center max-w-[1300px] m-auto'>
+        <Link className='font-bold text-[25px]' to={'/'}>
+          INFOBLENDER
+        </Link>
+        <div className='flex gap-[80px] items-center'>
+          <Link to={'/signup'}>Sign Up</Link>
+          <Link
+            className='text-[#DC7000] bg-[white] px-[28px] py-[10px] rounded'
+            to={'/login'}
+          >
+            Login
+          </Link>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+function Hero(){
+  return <section>
+    <div>
+
+    </div>
+    <div>
+      
+    </div>
+  </section>
+}
+
 
 function LandingPage() {
   return (
     <main>
-      <nav className='flex'>
-        <p className='font-bold'>INFOBLENDER</p>
-        <div>
-          <a href=''>Home</a>
-          <a href=''>Sign Up</a>
-          <a href=''>Login</a>
-        </div>
-      </nav>
+      <OutNavbar />
+      <Hero/>
       {/* Navbar */}
       {/* Hero Section */}
       {/* Intro paragraph section */}
